8616f15d1bb6385b2896e5cbcf9e508d9cbc8bac
saad <69530708+Saad042@users.noreply.github.com>
2022-11-20 16:00:46 +0500

    Implement playing one round of the game
    Additionally replaced game choice strings with constant variables to
    mitigate spelling errors.

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
+const ROCK = 'rock';
+const PAPER = 'paper';
+const SCISSORS = 'scissors';
+
 function getComputerChoice() {
     let num = getRandomInteger(0,2);
     let computerSelection;
     switch (num) {
         case 0:
-            computerSelection = 'rock';
+            computerSelection = ROCK;
             break;
         case 1:
-            computerSelection = 'paper';
+            computerSelection = PAPER;
             break;
         case 2:
-            computerSelection = 'scissors';
+            computerSelection = SCISSORS;
             break;
     }
     return computerSelection;
@@ -21,17 +25,59 @@ function getPlayerChoice() {
     while (!validInput) {
         playerSelection = prompt("Enter your choice.");
         playerSelection = playerSelection.toLowerCase();
-        if (playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors') {
+        if (playerSelection === ROCK || playerSelection === PAPER || playerSelection === SCISSORS) {
             validInput = true;
         }
     }
     return playerSelection; 
 }
 
+function playRound(playerSelection, computerSelection) {
+    let result;
+    if (playerSelection === ROCK) {
+        if (computerSelection === ROCK) {
+            result = 'Draw! Both chose Rock.';
+        }
+        else if (computerSelection === PAPER) {
+            result = 'You lose! Paper beats Rock.';
+        }
+        else if (computerSelection === SCISSORS) {
+            result = 'You win! Rock beats Scisscors.';
+        }
+    }
+
+    else if (playerSelection === PAPER) {
+        if (computerSelection === ROCK) {
+            result = 'You win! Paper beats Rock.';
+        }
+        else if (computerSelection === PAPER) {
+            result = 'Draw! Both chose Paper.';
+        }
+        else if (computerSelection === SCISSORS) {
+            result = 'You lose! Scissors beats Paper.';
+        }
+    }
+
+    else if (playerSelection === SCISSORS) {
+        if (computerSelection === ROCK) {
+            result = 'You lose! Rock beats Scisscors.';
+        }
+        else if (computerSelection === PAPER) {
+            result = 'You win! Scissors beats Paper.';
+        }
+        else if (computerSelection === SCISSORS) {
+            result = 'Draw! Both chose Scissors.';
+        }
+    }
+    return result;
+}
+
 function getRandomInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
   }
 
-
-console.log(getComputerChoice());
-console.log(getPlayerChoice());
+let computerSelection = getComputerChoice();
+let playerSelection = getPlayerChoice();
+console.log(computerSelection);
+console.log(playerSelection);
+console.log(playRound(playerSelection, computerSelection));
