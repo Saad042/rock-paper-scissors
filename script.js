bd244c151d0e8f24c9c0ad039e0b1442d0aa1b66
saad <69530708+Saad042@users.noreply.github.com>
2022-11-20 16:19:46 +0500

    Implement 5 round game
    Change round to also keep track of wins for both players.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const ROCK = 'rock';
 const PAPER = 'paper';
 const SCISSORS = 'scissors';
+let playerWins = 0;
+let computerWins = 0;
 
 function getComputerChoice() {
     let num = getRandomInteger(0,2);
@@ -40,30 +42,36 @@ function playRound(playerSelection, computerSelection) {
         }
         else if (computerSelection === PAPER) {
             result = 'You lose! Paper beats Rock.';
+            computerWins++;
         }
         else if (computerSelection === SCISSORS) {
             result = 'You win! Rock beats Scisscors.';
+            playerWins++;
         }
     }
 
     else if (playerSelection === PAPER) {
         if (computerSelection === ROCK) {
             result = 'You win! Paper beats Rock.';
+            playerWins++;
         }
         else if (computerSelection === PAPER) {
             result = 'Draw! Both chose Paper.';
         }
         else if (computerSelection === SCISSORS) {
             result = 'You lose! Scissors beats Paper.';
+            computerWins++;
         }
     }
 
     else if (playerSelection === SCISSORS) {
         if (computerSelection === ROCK) {
             result = 'You lose! Rock beats Scisscors.';
+            computerWins++;
         }
         else if (computerSelection === PAPER) {
             result = 'You win! Scissors beats Paper.';
+            playerWins++;
         }
         else if (computerSelection === SCISSORS) {
             result = 'Draw! Both chose Scissors.';
@@ -72,12 +80,26 @@ function playRound(playerSelection, computerSelection) {
     return result;
 }
 
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let computerSelection = getComputerChoice();
+        let playerSelection = getPlayerChoice(); 
+        console.log(playRound(playerSelection, computerSelection));
+    }
+
+    if (computerWins > playerWins) {
+        console.log(`Computer wins by ${computerWins} to ${playerWins}.`);
+    }
+    else if (playerWins > computerWins) {
+        console.log(`Player wins by ${playerWins} to ${computerWins}.`);
+    }
+    else if (playerWins === computerWins) {
+        console.log(`Its a draw with both having ${playerWins} wins.`);
+    }
+}
+
 function getRandomInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
-  }
+}
 
-let computerSelection = getComputerChoice();
-let playerSelection = getPlayerChoice();
-console.log(computerSelection);
-console.log(playerSelection);
-console.log(playRound(playerSelection, computerSelection));
+game();
